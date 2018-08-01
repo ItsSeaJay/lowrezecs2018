f0a1184d95e37016c5fefbe266a9f75ea108a02b
Unknown <b6010572@my.shu.ac.uk>
2018-08-01 12:53:03 +0000

    Merge branch 'develop'

--- src/entities/player.lua
+++ src/entities/player.lua
@@ -1,10 +1,12 @@
 local Entity = require "lib.concord.entity"
 local Position = require "src.components.position"
 local Sprite = require "src.components.sprite"
+local MovementController = require "src.components.movementcontroller"
 local PlayerEntity = Entity()
 
 -- A player has the following components:
-PlayerEntity:give(Position, 0, 0)
+PlayerEntity:give(Position, 0, 0) -- Start in the top-left
 PlayerEntity:give(Sprite, "res/placeholders/alien.png")
+PlayerEntity:give(MovementController, 4)
 
 return PlayerEntity
