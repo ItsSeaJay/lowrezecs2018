acbfbb6be55aa9772bb1d33b4ab7dfb9de24e920
Unknown <b6010572@my.shu.ac.uk>
2018-08-01 13:14:29 +0000

    Player is immobile

--- src/systems/Input.lua
+++ src/systems/Input.lua
@@ -3,23 +3,22 @@ local System = require "lib.concord.system"
 local MovementController = require "src.components.MovementController"
 local Position = require "src.components.Position"
 local InputSystem = System({
-	MovementController,
-	Position
+	MovementController
 })
 
-function InputSystem:handle(deltaTime)
+function InputSystem:update(deltaTime)
 	local up = love.keyboard.isDown("w")
 	local down = love.keyboard.isDown("s")
 	local left = love.keyboard.isDown("a")
 	local right = love.keyboard.isDown("d")
 
+	print(up)
+
 	for _, entity in ipairs(self.pool.objects) do
 		local controller = entity:get(MovementController)
 		local position = entity:get(Position)
 
-		if up then
-			position.y = position.y - controller.speed * deltaTime
-		end
+		position.y = position.y + 1
 	end
 end
 
