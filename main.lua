3f3ebbd800f67dd038e5e27bfbfe84f588281bba
Unknown <b6010572@my.shu.ac.uk>
2018-08-01 09:53:10 +0000

    The player can be moved through it's transform component

--- main.lua
+++ main.lua
@@ -1,29 +1,28 @@
 local concord = require "lib.concord"
 local maid64 = require "lib.maid64"
-local Player = require "src.entities.player"
-local Position = require "src.components.position"
+player = require "src.entities.player"
+Position = require "src.components.position"
 
 function love.load()
 	-- Initialise concord
 	concord.init()
 
+	alien = maid64.newImage("res/placeholders/alien.png")
+
 	-- Configure Maid64 to run at 64 pixels squared
 	maid64.setup(64)
-
-	alien = Player()
 end
 
 function love.update(deltaTime)
-	local position = alien:get(Position)
-	
-	position:move(1 * deltaTime, 1 * deltaTime)
+	player:get(Position):move(1 * deltaTime, 1 * deltaTime)
 end
 
 function love.draw()
 	-- Scale everything inbetween start() and finish()
 	-- to the target resolution
 	maid64.start()
-		love.graphics.draw(alien, 24, 24)
+		-- Draw a sprite at the player's current position
+		love.graphics.draw(alien, player:get(Position).x, player:get(Position).y)
 
 		-- Draw a circle at the mouse's position relative to scaling
 		love.graphics.circle("fill", maid64.mouse.getX(), maid64.mouse.getY(), 2)
