7001002f3fd3ef254383ae6f0d50f04c13cbacf6
Unknown <b6010572@my.shu.ac.uk>
2018-08-01 09:07:57 +0000

    Added config file and installed concord

--- main.lua
+++ main.lua
@@ -1,8 +1,14 @@
+local concord = require "lib.concord"
 local maid64 = require "lib.maid64"
 
 function love.load()
+	-- Initialise concord
+	concord.init()
+
 	-- Configure Maid64 to run at 64 pixels squared
 	maid64.setup(64)
+
+	alien = maid64.newImage("res/placeholders/alien.png")
 end
 
 function love.update(deltaTime)
@@ -13,6 +19,8 @@ function love.draw()
 	-- Scale everything inbetween start() and finish()
 	-- to the target resolution
 	maid64.start()
+		love.graphics.draw(alien, 24, 24)
+
 		-- Draw a circle at the mouse's position relative to scaling
 		love.graphics.circle("fill", maid64.mouse.getX(), maid64.mouse.getY(), 2)
 	maid64.finish()
---
+++ src/components/player.lua
@@ -0,0 +1,5 @@
+local concord
+
+local player = concord.entity()
+
+return player
